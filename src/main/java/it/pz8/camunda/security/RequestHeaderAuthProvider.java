242b4f843988a7cd5f09ab3254ab2798909c9039
Giuseppe Amato <giuseppe.amato@gmail.com>
2021-05-15 23:25:07 +0200

    Add support for groups and tenants as delimited string

--- src/main/java/it/pz8/camunda/security/RequestHeaderAuthProvider.java
+++ src/main/java/it/pz8/camunda/security/RequestHeaderAuthProvider.java
@@ -1,44 +1,54 @@
 package it.pz8.camunda.security;
 
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.rest.security.auth.AuthenticationProvider;
 import org.camunda.bpm.engine.rest.security.auth.AuthenticationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RequestHeaderAuthProvider implements AuthenticationProvider {
+/**
+ * @author Giuseppe Amato
+ *
+ */
+public class RequestHeaderAuthProvider extends AbstractHeaderAuthProvider {
 
-    private static final String USERNAME_DEFAULT_HEADER = "X-SSO-USERNAME";
-    private static final String GROUPS_DEFAULT_HEADER = "X-SSO-GROUPS";
-    private static final String TENANTS_DEFAULT_HEADER = "X-SSO-TENANTS";
+    protected static final String USERNAME_DEFAULT_HEADER = "X-SSO-USERNAME";
+    protected static final String GROUPS_DEFAULT_HEADER = "X-SSO-GROUPS";
+    protected static final String TENANTS_DEFAULT_HEADER = "X-SSO-TENANTS";
+    protected static final String USERNAME_HEADER_PARAM_KEY = "authprovider-usernameHeader";
+    protected static final String GROUPS_HEADER_PARAM_KEY = "authprovider-groupsHeader";
+    protected static final String TENANTS_HEADER_PARAM_KEY = "authprovider-tenantsHeader";
     
     private static final Logger LOGGER = LoggerFactory.getLogger(RequestHeaderAuthProvider.class);
 
     @Override
     public AuthenticationResult extractAuthenticatedUser(HttpServletRequest request, ProcessEngine engine) {
-        String usernameHeader = request.getServletContext().getInitParameter("authprovider-usernameHeader");
-        String groupsHeader = request.getServletContext().getInitParameter("authprovider-groupsHeader");
-        String tenantsHeader = request.getServletContext().getInitParameter("authprovider-tenantsHeader");
-        String username = request.getHeader(usernameHeader!=null && !"".equalsIgnoreCase(usernameHeader)?usernameHeader:USERNAME_DEFAULT_HEADER);
-        Enumeration<String> groups = request.getHeaders(groupsHeader!=null && !"".equalsIgnoreCase(groupsHeader)?groupsHeader:GROUPS_DEFAULT_HEADER);
-        Enumeration<String> tenants = request.getHeaders(tenantsHeader!=null && !"".equalsIgnoreCase(tenantsHeader)?tenantsHeader:TENANTS_DEFAULT_HEADER);
-        AuthenticationResult authenticationResult = (username!=null && !"".equalsIgnoreCase(username))?
+        String usernameHeader = request.getServletContext().getInitParameter(USERNAME_HEADER_PARAM_KEY);
+        String groupsHeader = request.getServletContext().getInitParameter(GROUPS_HEADER_PARAM_KEY);
+        String tenantsHeader = request.getServletContext().getInitParameter(TENANTS_HEADER_PARAM_KEY);
+        
+        String username = request.getHeader((usernameHeader!=null && !usernameHeader.isEmpty())?usernameHeader:USERNAME_DEFAULT_HEADER);
+        List<String> groups = getClaim(groupsHeader, GROUPS_DEFAULT_HEADER, isRolesAsList(request), getRolesDelimiter(request), request);
+        List<String> tenants = getClaim(tenantsHeader, TENANTS_DEFAULT_HEADER, isTenantsAsList(request), getTenantsDelimiter(request), request);
+
+        AuthenticationResult authenticationResult = (!username.isEmpty())?
                 AuthenticationResult.successful(username):
-                AuthenticationResult.unsuccessful();  
-        if (groups!=null && groups.hasMoreElements()) {
-            authenticationResult.setGroups(Collections.list(groups));
+                AuthenticationResult.unsuccessful();
+        if (groups!=null && !groups.isEmpty()) {
+            authenticationResult.setGroups(groups);
         }
-        if (tenants!=null && tenants.hasMoreElements()) {
-            authenticationResult.setTenants(Collections.list(tenants));
+        if (tenants!=null && !tenants.isEmpty()) {
+            authenticationResult.setTenants(tenants);
         }
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug(String.format("Username: %s - Groups: %s - Tenants: %s", authenticationResult.getAuthenticatedUser(), authenticationResult.getGroups(), authenticationResult.getTenants()));
+            LOGGER.debug(String.format("Username: %s - Groups: %s - Tenants: %s", 
+                    authenticationResult.getAuthenticatedUser(), authenticationResult.getGroups(), authenticationResult.getTenants()));
         }
         return authenticationResult;
     }
@@ -48,4 +58,16 @@ public class RequestHeaderAuthProvider implements AuthenticationProvider {
         // Do nothing.
     }
 
+    private List<String> getClaim(String headerName, String defaultHeaderName, boolean isList, String delimiter, HttpServletRequest request) {
+        String currentHeaderName = (headerName!=null && !headerName.isEmpty())?headerName:defaultHeaderName;
+        List<String> results = null;
+        if (isList) {
+            results = Collections.list(request.getHeaders(currentHeaderName));
+        } else {
+            if (request.getHeader(currentHeaderName)!=null) {
+                results = Arrays.asList(request.getHeader(currentHeaderName).split(delimiter));
+            }
+        }
+        return results;
+    }
 }
